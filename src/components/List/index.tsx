44a8bc6304e0ec0e07fc7fdbe9716610faf40ddd
João Pedro <jvieirasousa121@hotmail.com>
2023-10-04 17:40:45 -0300

    fim do curso

---
+++ src/components/List/index.tsx
@@ -0,0 +1,30 @@
+import { memo } from "react";
+
+import Item from "./Item";
+import { ITarefa } from "../../types/tarefa";
+
+import style from './List.module.scss'
+
+interface Props {
+  tarefas: ITarefa[];
+  selecionaTarefa: (tarefaSelecionada: ITarefa) => void
+}
+
+function List({ tarefas, selecionaTarefa }: Props) {
+  return (
+    <aside className={style.listaTarefas}>
+      <h2> Estudos do dia </h2>
+      <ul>
+        {tarefas.map((item) => (
+          <Item
+            selecionaTarefa={selecionaTarefa}
+            key={item.id}
+            {...item}
+          />
+        ))}
+      </ul>
+    </aside>
+  )
+}
+
+export default memo(List);
